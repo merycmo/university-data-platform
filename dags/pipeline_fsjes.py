9329217a939f887dfa017ded607d7c2167d2efa0
ramafiona04 <mbazogheramafiona04@gmail.com>
2026-06-19 00:29:23 +0100

    feat: switch to FMPM faculty pipeline

--- dags/pipeline_fsjes.py
+++ dags/pipeline_fsjes.py
@@ -1,11 +1,12 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from scraper.university_scraper import scrape_university
 
 scrape_university(
-    start_url  = "https://fsjes.uca.ma/",
+    start_url  = "https://fmpm.uca.ma/",
     university = "cadi_ayyad",
-    faculty    = "fsjes",
+    faculty    = "fmpm",
     max_depth  = 3
 )
