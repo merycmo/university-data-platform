d34a6d775cd775085aa34b934886356f820da1b2
ramafiona04 <mbazogheramafiona04@gmail.com>
2026-06-19 00:37:42 +0100

    feat: rename pipeline fsjes to fmpm

---
+++ dags/pipeline_fsac.py
@@ -0,0 +1,12 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from scraper.university_scraper import scrape_university
+
+scrape_university(
+    start_url  = "https://fsjes.uca.ma/",
+    university = "cadi_ayyad",
+    faculty    = "fsjes",
+    max_depth  = 4
+)
