b7155120010459b3920c93a995d923ca4af2d76a
hafsamasrour <hafsamasrour03@gmail.com>
2026-06-19 19:10:40 +0100

    pipeline FSSM complet scraping + API + fichiers

--- dags/pipeline_fssm.py
+++ dags/pipeline_fssm.py
@@ -7,6 +7,8 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from scraper.university_scraper import scrape_university
+from ingestion.ingest_api import ingest_faculty
+from ingestion.ingest_file import run_file_ingestion
 import logging
 
 logging.basicConfig(level=logging.INFO)
@@ -17,15 +19,29 @@ def run_pipeline():
     logger.info("PIPELINE FSSM - cadi_ayyad")
     logger.info("=" * 50)
 
-    stats = scrape_university(
+    # Étape 1 - Scraping web
+    logger.info("Étape 1 : Scraping web...")
+    stats_web = scrape_university(
         start_url  = "https://www.uca.ma/fssm/fr",
         university = "cadi_ayyad",
         faculty    = "FSSM",
         max_depth  = 3
     )
+    logger.info(f"Scraping terminé : {stats_web}")
 
-    logger.info(f"Pipeline FSSM terminé : {stats}")
-    return stats
+    # Étape 2 - Ingestion API OpenAlex
+    logger.info("Étape 2 : Ingestion API OpenAlex...")
+    ingest_faculty("Cadi Ayyad", "FSSM")
+
+    # Étape 3 - Ingestion fichiers PDF
+    logger.info("Étape 3 : Ingestion fichiers PDF...")
+    stats_file = run_file_ingestion(
+        university = "cadi_ayyad",
+        faculty    = "FSSM"
+    )
+    logger.info(f"Ingestion fichiers terminée : {stats_file}")
+
+    logger.info("Pipeline FSSM terminé !")
 
 if __name__ == "__main__":
     run_pipeline()
